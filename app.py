cba70a944dd4cea02fcab4f98c9fc93d493fc026
wjm07 <utenacasshern@gmail.com>
2018-06-04 21:43:05 +0200

    get and add cases, refactor useful functions, schema fixes

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify, request, redirect, url_for, session, flash
+from flask_mysqldb import MySQL, MySQLdb
 from passlib.hash import pbkdf2_sha256
 from case_info import case_info
-from extensions import mysql
+from extensions import *
 import getpass, datetime, urllib.request
 
 app = Flask(__name__)
@@ -12,7 +13,7 @@ app.register_blueprint(case_info)
 
 @app.route('/')
 def index():
-    if _check_database():
+    if check_database_():
         return jsonify({'status':'ready'})
     else:
         return jsonify({'status':'error'})
@@ -28,8 +29,48 @@ def create_db():
             cursor.execute(query)
         mysql.connection.commit()
         return jsonify({'status':'success'})
-    except:
-        return jsonify({'status':'error'}), 400
+    except MySQLdb.Error as e:
+        print(e)
+        return jsonify({"status":"error",}), 400
+
+@app.route('/cases/get', methods=(['GET']))
+def get_cases():
+    return select_query_result_({}, 'cases')
+
+@app.route('/cases/add', methods=(['POST']))
+def add_case():
+    # TODO Safe error handling
+    # Patient details, history and hospital_id MUST be submitted
+    cursor = mysql.connection.cursor()
+
+    cols_cases = get_cols_('cases')
+    args_cases = get_args_(cols_cases, request.form)
+
+    add_params = add_(args_cases)
+    add_query = 'insert into cases ' + add_params[0]
+    cursor.execute(add_query, add_params[1])
+    cursor.execute('select last_insert_id()')
+    result = cursor.fetchall()
+    case_id = result[0]['last_insert_id()']
+
+    for info_table in ['case_histories', 'case_assessments']:
+        cols_table = get_cols_(info_table)
+        args_table = get_args_(cols_table, request.form)
+        args_table['case_id'] = case_id
+        add_params = add_(args_table)
+        add_query = 'insert into {} '.format(info_table) + add_params[0]
+        cursor.execute(add_query, add_params[1])
+
+    hospital_query = 'insert into case_hospitals (case_id, hospital_id) values (%s, %s)'
+    cursor.execute(hospital_query, (case_id, request.form.get('hospital_id')))
+
+    for info_table in ['case_eds', 'case_radiologies', 'case_managements']:
+        add_query = 'insert into {} '.format(info_table) + '(case_id) values (%s)'
+        cursor.execute(add_query, (case_id,))
+        
+    mysql.connection.commit()
+
+    return jsonify({'status':'success'})
 
 if __name__ == '__main__':
     app.run(debug = True)
