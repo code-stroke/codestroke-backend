04bea5227fa3e794b4c8c85d9dd595acd2742ca8
PythonAnywhereCodestroke <codestroke@24dcaea7d349>
2018-06-24 00:18:29 +0000

    debugged datetime formatting with list to map and check instance datetime before date

--- hooks.py
+++ hooks.py
@@ -3,14 +3,14 @@ import datetime
 def fetch(result):
     # datetimes - convert to sensible format
     rows = list(result)
-    rows= map(lambda x: k:_process_fetch(k,v) for (k,v) in x.items(), rows)
+    rows= list(map(lambda x: {k:_process_fetch(k,v) for (k,v) in x.items()}, rows))
     return rows
 
 def _process_fetch(key, value):
-    if isinstance(value, datetime.date):
+    if isinstance(value, datetime.datetime):
+        return value.strftime("%Y-%m-%d %H:%M")
+    elif isinstance(value, datetime.date):
         return value.isoformat()
-    elif isinstance(value, datetime.datetime):
-        return value.strftime("%y-%m-%d %H-%M")
     else:
         return value
 
