38c4db0aafbda4d9da0f821d6cb87414a408c547
wjmn <Jamin.Wu07@gmail.com>
2018-06-24 10:02:56 +1000

    added date and datetime format hook to deal with returned format for times

--- hooks.py
+++ hooks.py
@@ -1,5 +1,19 @@
+import datetime
+
 def fetch(result):
-    return result
+    # datetimes - convert to sensible format
+    rows = list(result)
+    rows= map(lambda x: k:_process_fetch(k,v) for (k,v) in x.items(), rows)
+    return rows
+
+def _process_fetch(key, value):
+    if isinstance(value, datetime.date):
+        return value.isoformat()
+    elif isinstance(value, datetime.datetime):
+        return value.strftime("%y-%m-%d %H-%M")
+    else:
+        return value
 
 def put(info_table, case_id, qargs):
+    # status changed ->also change status time
     return qargs
