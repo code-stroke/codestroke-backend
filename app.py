2472262748d2634dfa2e05bc6979927f0779e591
wjmn <Jamin.Wu07@gmail.com>
2018-06-26 19:42:21 +1000

    Small notification and function changes to suit workflow.
    Registering in ED no longer changes status to active, will rely on paramedic app
    dropoff to send a PUT request to change status for this instead.
    
    Unnecessary table information getting in add_case function removed to suit
    per-page sending of data from paramedic app.

--- app.py
+++ app.py
@@ -53,12 +53,10 @@ def add_case():
     info_tables = ['case_histories', 'case_assessments',
                    'case_eds', 'case_radiologies', 'case_managements']
 
-    # Will accept parameters from ANY of the case info table (incl. ed)
+    args_table = {'case_id': case_id}
+    add_params = ext.add_(args_table)
+
     for info_table in info_tables:
-        cols_table = ext.get_cols_(info_table)
-        args_table = ext.get_args_(cols_table, request.get_json())
-        args_table['case_id'] = case_id
-        add_params = ext.add_(args_table)
         add_query = 'insert into {} '.format(info_table) + add_params[0]
         cursor.execute(add_query, add_params[1])
 
@@ -68,7 +66,7 @@ def add_case():
     mysql.connection.commit()
 
     notify.add_message('case_incoming', case_id, {'eta_mins': True}) # PLACEHOLDER for eta
-    
+
     return jsonify({'success': True, 'case_id': case_id})
 
 @app.route('/cases/<int:case_id>/', methods=(['DELETE']))
