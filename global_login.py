ce147745d3dcb149a1473ac19f54dd7c19a4418a
wjmn <Jamin.Wu07@gmail.com>
2018-07-21 19:02:21 +1000

    Global Login functionality added.

---
+++ global_login.py
@@ -0,0 +1,19 @@
+from flask import request, jsonify
+from flask import current_app as app
+from passlib.hash import pbkdf2_sha256
+
+def check_auth(username, password):
+    if pbkdf2_sha256.verify(password, app.config['GLOBAL_PW_HASH']):
+        return True
+    return False
+
+def requires_global_login(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return jsonify({'success': False,
+                            'error_type': 'auth',
+                            'debugmsg': 'Authentication failed',})
+        return f(*args, **kwargs)
+    return decorated
